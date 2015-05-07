c2867ad729b9434f2da9dfc6e8b841db4fcc1a30
ashaffs <ajshaffstall@fullsail.edu>
2015-05-06 23:54:07 -0700

    Changes made for class
    These changes were made for the video prompt and alert

--- AssociatingJS/JS/script.js
+++ AssociatingJS/JS/script.js
@@ -2,6 +2,8 @@
  * Created by andrewshaffstall1 on 5/6/15.
  */
 
-var a = 3;
-a ++ ; //a = a + 1 // a += 1
-console.log(a);
+var width = prompt("We are calculating the area of a rectanlge. \nPlease enter the width.");
+var height = prompt("Please enter the height");
+var area = width * height; //calculates with that info
+//console.log(area); //prints it out to console
+alert("the area of your rectangle is " + area + " sq feet");
