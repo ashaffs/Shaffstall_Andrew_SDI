ba9d56dc75ee0f1c63c543301f17800601e43c3a
ashaffs <ajshaffstall@fullsail.edu>
2015-05-06 23:15:35 -0700

    Changes made for class
    These are changes that were made for the video assignment operators

--- AssociatingJS/JS/script.js
+++ AssociatingJS/JS/script.js
@@ -2,9 +2,6 @@
  * Created by andrewshaffstall1 on 5/6/15.
  */
 
-var areaCode = 623
-var firstPart = 399
-var secPart = 7691
-
-var phoneNo = "(" + String(areaCode) + ")" + String(firstPart) + "-" + String(secPart);
-console.log(phoneNo);
+var a = 3;
+a ++ ; //a = a + 1 // a += 1
+console.log(a);
