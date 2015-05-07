90b4f34da0449c761e2d529f86bea8bb0cc6905a
ashaffs <ajshaffstall@fullsail.edu>
2015-05-06 22:47:12 -0700

    These are changes for class
    These changes are for the video about the order of operations

--- AssociatingJS/JS/script.js
+++ AssociatingJS/JS/script.js
@@ -2,5 +2,11 @@
  * Created by andrewshaffstall1 on 5/6/15.
  */
 
-var remainder = 32 % 10;
-console.log(remainder);
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
+
+//the average is the sum of the items by the number of items
+var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
+console.log(average);
