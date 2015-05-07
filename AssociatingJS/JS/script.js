c0a269fa21be0413fe8c2b70d6f2a3f44d3b4b70
ashaffs <ajshaffstall@fullsail.edu>
2015-05-06 22:28:51 -0700

    Changes made for Class
    Changes made for the video about example expressions

--- AssociatingJS/JS/script.js
+++ AssociatingJS/JS/script.js
@@ -2,9 +2,6 @@
  * Created by andrewshaffstall1 on 5/6/15.
  */
 
-var a = 2; //sets up our variable a and defines it with 2
-var b;
-b = a + 3; //add 3 to a
-
-console.log(b);
-
+var yearBorn = 1994;
+var age = 2015 - yearBorn;
+console.log(age);
