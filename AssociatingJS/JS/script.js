411d210668c5939f69767060e007b24609ec9e0b
ashaffs <ajshaffstall@fullsail.edu>
2015-05-06 22:41:17 -0700

    Changes made for class
    Changes made for the video about using modulo

--- AssociatingJS/JS/script.js
+++ AssociatingJS/JS/script.js
@@ -2,8 +2,5 @@
  * Created by andrewshaffstall1 on 5/6/15.
  */
 
-//area of the triangle is half of the width times the height
-var width = 4;
-var height = 5;
-var area = width * height/2
-console.log(area);
+var remainder = 32 % 10;
+console.log(remainder);
