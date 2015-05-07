938f03da4cb3dad939c46188c68447f479f99d97
ashaffs <ajshaffstall@fullsail.edu>
2015-05-07 00:20:24 -0700

    Expressions Worksheet Problem 2
    This is the programmed solution to Problem two on the Expressions
    Worksheet

--- Expressions Worksheet/js/script.js
+++ Expressions Worksheet/js/script.js
@@ -6,3 +6,12 @@ var humanAge = 5; //How old sparky is in human years
 var dogYears = 7; //This is how many dog years per 1 human year
 var sparkyAge = humanAge * dogYears; //this calculates how old sparky is in dog years
 console.log("Sparky is " + humanAge + " human years old which is " + sparkyAge + " in dog years."); //this displays sparkys age to the user
+
+//Problem #2
+var slices = 8; //number of slices per pizza
+var people = 30; //number of people at the party
+var pizzas = 10; //number of pizzas ordered
+
+var slicesPerPerson = pizzas * slices / people; //this calculates how many slices of pizza each person at the party got
+
+console.log("Each person ate " + slicesPerPerson + " slices of pizza at the party"); //This displays how many slices each person at the party got
