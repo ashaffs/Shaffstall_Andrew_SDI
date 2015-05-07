6fe13a5c39ffbc8590b002ca4539ffdf88283bba
ashaffs <ajshaffstall@fullsail.edu>
2015-05-06 22:24:45 -0700

    Changes made for Class
    Changes made for video about basic expressions

--- AssociatingJS/JS/script.js
+++ AssociatingJS/JS/script.js
@@ -2,11 +2,9 @@
  * Created by andrewshaffstall1 on 5/6/15.
  */
 
-var muppetNames = ["Kermit", "Miss Piggy"]; //0,1,2,3
+var a = 2; //sets up our variable a and defines it with 2
+var b;
+b = a + 3; //add 3 to a
 
-muppetNames[1] = "Rizzo"; //changes index #1 to Rizzo
-muppetNames[2] = "Fozzy";
+console.log(b);
 
-var num = 2
-
-console.log(muppetNames[num]); //lets me see whats in the index #1
