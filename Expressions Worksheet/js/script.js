fbe846f154d8dc0fc0b1688db2291894cdcf64e0
ashaffs <ajshaffstall@fullsail.edu>
2015-05-07 00:08:18 -0700

    Example for Expressions Worksheet
    This is the Example for the Expressions worksheet

---
+++ Expressions Worksheet/js/script.js
@@ -0,0 +1,6 @@
+//Andrew Shaffstall Expressions Worksheet
+
+var width = "3"; //this is the width of the rectangle
+var height = "5"; //this is the height of the rectangle
+var area = width * height; //area of rectangle is equal to width times height
+console.log("The area of the rectanlge is" + " " + area); //this displays the answer in the console
