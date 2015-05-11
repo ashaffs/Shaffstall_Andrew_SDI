a826204815fa6b75d10db137c37d2931d5d8716f
ashaffs <ajshaffstall@fullsail.edu>
2015-05-11 16:02:09 -0700

    Expressions Assignment
    This is the changes made to add phone cost

--- Shaffstall_Andrew_Expression_Assingment/js/script.js
+++ Shaffstall_Andrew_Expression_Assingment/js/script.js
@@ -5,4 +5,9 @@ var accPrice = [25, 35, 50, 100, 150]; //these are the cost of each accessory
 
 var total = accPrice[0] + accPrice[1] + accPrice[2] + accPrice[3] + accPrice[4]; //this is the total of all the accessories
 
-console.log(total); //this was a test to see if the link will work with the program
+//console.log(total); //this was a test to see if the link will work with the program
+
+var phonePrice  =  649.99
+
+var equipmentInstallment = (total + phonePrice)/24
+
