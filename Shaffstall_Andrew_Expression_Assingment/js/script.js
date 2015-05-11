0a58f2755583e6c0917827f25bba2583acb8a06e
ashaffs <ajshaffstall@fullsail.edu>
2015-05-11 15:57:58 -0700

    Expressions Assignment
    This got the total of the accessories into the console in the
    expressions assignment

---
+++ Shaffstall_Andrew_Expression_Assingment/js/script.js
@@ -0,0 +1,8 @@
+//Andrew Shaffstall 5/11/15 Expression Assingment
+
+
+var accPrice = [25, 35, 50, 100, 150]; //these are the cost of each accessory
+
+var total = accPrice[0] + accPrice[1] + accPrice[2] + accPrice[3] + accPrice[4]; //this is the total of all the accessories
+
+console.log(total); //this was a test to see if the link will work with the program
