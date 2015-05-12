65e63cb8d0cf234a349e55716bac33901d998b44
ashaffs <ajshaffstall@fullsail.edu>
2015-05-11 17:02:50 -0700

    Expressions Assignment EIP test
    This tested the console to see if it would run the EIP  program

--- Shaffstall_Andrew_Expression_Assingment/js/script.js
+++ Shaffstall_Andrew_Expression_Assingment/js/script.js
@@ -7,7 +7,10 @@ var total = accPrice[0] + accPrice[1] + accPrice[2] + accPrice[3] + accPrice[4];
 
 //console.log(total); //this was a test to see if the link will work with the program
 
-var phonePrice  =  649.99
+var phonePrice  =  649.99; //this is the full price of the phone
 
-var equipmentInstallment = (total + phonePrice)/24
+var equipmentInstallment = (total + phonePrice)/24; //this is the total of the equipment installment plan divided up over 24 months
+
+console.log(equipmentInstallment); //This will test and make sure that the program works
+
 
