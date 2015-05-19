5ab0fce4bad5672b59e65a09a5f6c8f73250e4f5
ashaffs <ajshaffstall@fullsail.edu>
2015-05-19 00:01:10 -0700

    Changes made for class
    These are changes that were made for the file about conditionals and
    involves else for the first time.

--- Conditional/JS/script.js
+++ Conditional/JS/script.js
@@ -10,4 +10,6 @@ var minHeight = 48;
 if(kidHeight > minHeight){
     //code performed if condition is true
     console.log("You can ride the coaster!");
-}
+}else{
+    console.log("Sorry kid, you've got some growing to do first!")
+};
