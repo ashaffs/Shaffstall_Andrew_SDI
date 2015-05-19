8559676af1a072369eb43927b69a0953cfc36b19
ashaffs <ajshaffstall@fullsail.edu>
2015-05-18 23:51:39 -0700

    Changes made for class
    These is a new file based around conditionals

---
+++ Conditional/JS/script.js
@@ -0,0 +1,12 @@
+/**
+ * Created by andrewshaffstall1 on 5/18/15.
+ */
+
+var oldEnough = true;
+
+//if the child is old enough, print to the console "you can ride!"
+if(oldEnough){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+}
+console.log("What comes after");
