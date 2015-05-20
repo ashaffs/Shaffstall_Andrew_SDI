cab118c440bfd75dddeae987cb8d8438a500146d
ashaffs <ajshaffstall@fullsail.edu>
2015-05-19 21:43:21 -0700

    Changes made for class
    There have been changes made for conditionals and Ternary operators

--- Conditional/JS/script.js
+++ Conditional/JS/script.js
@@ -2,13 +2,9 @@
  * Created by andrewshaffstall1 on 5/18/15.
  */
 
-var budget = 100;
-var androidPhone = 199.99;
-var wonLottery = true;
+var age = 11;
+var book;
 
-//if the price of the phone is less than our budget and if our paycheck is over 300
-if(androidPhone < budget || wonLottery){
-    console.log("We can buy the phone!");
-}else{
-    console.log("No phone for you!!");
-};
+//if the child is under 10, they get Green Eggs and Ham, otherwise they get The Time Machine
+book = (age < 10) ? "Green Eggs and Ham" : "The Time Machine";
+console.log(book);
