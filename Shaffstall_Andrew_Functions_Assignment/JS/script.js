70830895303f1407341274ff721240615dd05bca
ashaffs <ajshaffstall@fullsail.edu>
2015-05-28 19:24:06 -0700

    This added the second function
    This is the second function that calculates the horizontal pip

--- Shaffstall_Andrew_Functions_Assignment/JS/script.js
+++ Shaffstall_Andrew_Functions_Assignment/JS/script.js
@@ -1,6 +1,6 @@
 //Andrew Shaffstall 5-28-15 Functions Assignment
 
-var vertilcle = verticalPpi(1440, 10); //this shows the virtical length and resolution and includes the arguments
+var vertilcle = verticalPpi(1440, 10); //this shows the vertical length and resolution and includes the arguments
 
 function verticalPpi(heightPixels, heightInches){//this is the start of the function that includes the parameters
     var heightPpi = heightPixels / heightInches;//this is the equation that does the calculating
@@ -8,4 +8,13 @@ function verticalPpi(heightPixels, heightInches){//this is the start of the func
     return heightPpi; //this spits the answer back outside of the function
 }
 //this function calculates the vertical ppi of the screen
+
+var horizontal = horizontalPpi(2560, 15);//this shows the horizontal length and resolution and inclused the arguements
+
+function horizontalPpi(widthPixels, widthInches){//this is the start of the function that includes the parameters
+    var widthPpi = widthPixels / widthInches;//this is the equation that does the calculating
+    console.log("YOur horizontal ppi is " + widthPpi);//this displays the answer on the console
+    return widthPpi; //this spits the answer back outside of the function
+}
+
 
